056f549c4750212add72f7e1c5b89c12b490f5bd
Kelu Diao <Kelu@pal-nat186-101-170.itap.purdue.edu>
2015-11-15 21:01:45 -0500

    initial commit

---
+++ api.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+	"log"
+	"io/ioutil"
+)
+
+func main() {
+	var url string
+	fmt.Scan(&url)
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	fmt.Println(body)
+}
