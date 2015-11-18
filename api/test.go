83d8eabe7908fa92180c9fc3ae1471f677d79552
KeluDiao <diaokelu@gmail.com>
2015-11-17 23:16:53 -0500

    Removed an uncessary file

--- api/test.go
+++ api/test.go
@@ -1,13 +1,14 @@
 package gotube
 
 import (
+	"os"
 	"testing"
 )
 
 func GetVideoListTesting(t *testing.T) {
 	//Testing valid video
 	testTitle := "TEST VIDEO"
-	cl := Client {VideoRepository: tests}
+	cl := Client {VideoRepository: "videos"}
 	vl, err := cl.GetVideoListFromId("C0DPdy98e4c")	//Get test video
 	if err != nil {
 		t.Fatalf(err)
@@ -26,4 +27,24 @@ func GetVideoListTesting(t *testing.T) {
 	}
 }
 
-func DownloadTesting(t * testing.T)
+func DownloadTesting(t * testing.T) {
+	cl := Client {VideoRepository: "videos"}
+	vl, err := cl.GetVideoListFromId("C0DPdy98e4c")	//Get test video
+	err = vl.Filter("medium", "video/mp4")
+	if err != nil {
+		t.Fatalf(err)
+	}
+	if len(vl.videos) != 1 {
+		t.Fatalf("Expected 1 videos after filtering, got %v", len(vl.videos))
+	}
+	//Download video into ./videos
+	err = vl.Download(cl, "", "")
+	if err != nil {
+		t.Fatalf(err)
+	}
+	//Remove repository ./videos
+	err = os.Remove("videos")
+	if err != nil {
+		t.Fatalf(err)
+	}
+}
